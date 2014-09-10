39ebf6d38dca63347a550c8e992b8bfa0630f7a5
vpetit <victor.petit@etudiant.univ-rennes1.fr>
2014-09-10 11:36:06 +0200

    [INIT PROJECT]

---
+++ src/main/java/istic/m2gl/App.java
@@ -0,0 +1,17 @@
+package istic.m2gl;
+
+// TODO: Auto-generated Javadoc
+/**
+ * Hello world!.
+ */
+public class App {
+	
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
